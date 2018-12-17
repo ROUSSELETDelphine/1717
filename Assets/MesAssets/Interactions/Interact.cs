98bf85f49b804fc94c2f022d83b1315483725b47
Ombeline Leclerc-Istria <o.leclerc.istria@gmail.com>
2018-12-17 11:08:11 -0500

    Porte finale modifiée

---
+++ Assets/MesAssets/Interactions/Interact.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public GameObject interaction_text;
+    
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Team2"))
+        {
+            if (interaction_text != null)
+            {
+                interaction_text.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Team2"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Lancement de l'animation
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Team2"))
+        {
+            if (interaction_text != null)
+            {
+                interaction_text.SetActive(false);
+            }
+        }
+    }
+}
