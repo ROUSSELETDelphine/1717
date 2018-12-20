f0f874094cc2e6856bc4913a9233bca6fb23fff3
Ombeline Leclerc-Istria <o.leclerc.istria@gmail.com>
2018-12-20 18:30:12 -0500

    Sons menu principal + lancement menu-cinematique-jeu

---
+++ Assets/MesAssets/Audio_Pointer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Audio_Pointer : MonoBehaviour, IPointerEnterHandler
+{
+
+    public AudioSource source;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        source.PlayOneShot(source.clip);
+    }
+
+}
