ce0b131bc99dc4c9880c5a6e4dff5d7b7d4d4a8f
Ombeline Leclerc-Istria <o.leclerc.istria@gmail.com>
2018-12-20 16:05:55 -0500

    Cinematiques

---
+++ Assets/launch_game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class launch_game : MonoBehaviour {
+    
+    public VideoPlayer videoPlayer;
+    private bool started = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        if (videoPlayer.isPlaying && !started)
+        {
+            started = true;
+        }
+
+        if (!videoPlayer.isPlaying && started)
+        {
+            SceneManager.LoadScene(3);
+        }
+
+
+    }
+
+}
