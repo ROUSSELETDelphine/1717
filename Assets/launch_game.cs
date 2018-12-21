c69a5f1e8714cef254fe2e947bb2ef5a822078fb
Ombeline Leclerc-Istria <o.leclerc.istria@gmail.com>
2018-12-20 19:08:14 -0500

    Space to skip

--- Assets/launch_game.cs
+++ Assets/launch_game.cs
@@ -7,10 +7,19 @@ using UnityEngine.SceneManagement;
 public class launch_game : MonoBehaviour {
     
     public VideoPlayer videoPlayer;
+    public GameObject skipText;
     private bool started = false;
+    private int maxIter = 120;
+    private int ctr;
+
+    private void Start()
+    {
+        ctr = 0;
+    }
 
     void Update()
     {
+        ctr++;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(3);
@@ -26,6 +35,10 @@ public class launch_game : MonoBehaviour {
             SceneManager.LoadScene(3);
         }
 
+        if(ctr == maxIter)
+        {
+            skipText.SetActive(false);
+        }
 
     }
 
